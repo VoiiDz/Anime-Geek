1793934c52e3ea983cea64c134af46ffa72e6994
VoiiDz <moncifjb@gmail.com>
2021-02-15 18:10:21 +0100

    add the request files

---
+++ src/requests.js
@@ -0,0 +1,12 @@
+
+const requests = {
+    getTopRated: "/top/anime/1/airing",
+    getUpcoming: "/top/anime/1/upcoming",
+    getActionAn: "/genre/anime/1/1",
+    getHorrorAn: "/genre/anime/14/1",
+    getComedyAn: "/genre/anime/4/1",
+    getAdvAn: "/genre/anime/2/1",
+    getRomance: "/genre/anime/22/1"
+}
+
+export default requests
