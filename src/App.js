abafe3c2b761b4d0bee1725b00d5db5b7965c368
VoiiDz <moncifjb@gmail.com>
2021-02-15 22:27:46 +0100

    final Row.css touches

--- src/App.js
+++ src/App.js
@@ -6,6 +6,11 @@ function App() {
   return (
     <div className="App">
       <Row title="TOP Animes" fetchUrl={requests.getTopRated} />
+      <Row title="Upcoming Animes" fetchUrl={requests.getUpcoming} />
+      <Row title="Action Animes" fetchUrl={requests.getActionAn} />
+      <Row title="Horror Animes" fetchUrl={requests.getHorrorAn} />
+      <Row title="Adventure Animes" fetchUrl={requests.getAdvAn} />
+      <Row title="Comedy Animes" fetchUrl={requests.getComedyAn} />
     </div>
   );
 }
--- src/components/Row.js
+++ src/components/Row.js
@@ -9,20 +9,20 @@ function Row({ fetchUrl, title }) {
     useEffect(() => {
         const fetchData = async () => {
             const request = await axios.get(fetchUrl);
-            setAnimes(request.data.top.slice(0, 20));
+            setAnimes(request.data.top?.slice(0, 20) || request.data.anime?.slice(0, 20));
         }
         fetchData();
     }, []);
 
-    const handleScroll = () => {
-        ref.current.scrollLeft += 20;
+    const handleScroll = (offset) => {
+        ref.current.scrollLeft += offset;
     }
 
     return (
         <div className="row">
             <h2>{title}</h2>
             <div className="row-container">
-                <div className="btn btn-left" onClick={handleScroll}>
+                <div className="btn btn-left" onClick={() => handleScroll(-100)}>
                     <img src="../left-arrow.svg" alt=""/>
                 </div>
                 <div className="row-imgs" ref={ref}>
@@ -35,7 +35,7 @@ function Row({ fetchUrl, title }) {
                         />
                     )}
                 </div>
-                <div className="btn btn-right" onClick={handleScroll}>
+                <div className="btn btn-right" onClick={() => handleScroll(100)}>
                     <img src="../right-arrow.svg" alt=""/>
                 </div>
             </div>
